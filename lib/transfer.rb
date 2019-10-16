2178c363acd58489d8a0db38117c4755ef14646b
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-16 20:53:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
       @sender.balance -= @amount 
       @receiver.balance += @amount 
     else 
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end 
   end
