d0cb4b784c1405dd39dfb9c3f77de3deb81c59af
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-16 20:50:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,6 @@ class Transfer
   end
   
   def execute_transaction 
-    binding.pry
     if valid? && @status == "pending" && @sender.balance >= amount
       @status = "complete"
       @sender.balance -= @amount 
